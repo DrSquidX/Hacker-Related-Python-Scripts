223ec1e1f8a6c63694debd69551e54ab9025ddc7
DrSquidX <68380622+DrSquidX@users.noreply.github.com>
2021-01-24 10:32:13 +0100

    Update keyloggerserver.py

--- keyloggerserver.py
+++ keyloggerserver.py
@@ -5,6 +5,16 @@ ip = '192.168.0.115'
 port = 1234
 s.bind((ip, port))
 connlist = []
+print("  _  __            _                                    _____                            ___    ___  ")
+print(" | |/ /           | |                                  / ____|                          |__ \  / _ \ ")
+print(" | ' / ___ _   _  | |     ___   __ _  __ _  ___ _ __  | (___   ___ _ ____   _____ _ __     ) || | | |")
+print(" |  < / _ \ | | | | |    / _ \ / _` |/ _` |/ _ \ '__|  \___ \ / _ \ '__\ \ / / _ \ '__|   / / | | | |")
+print(" | . \  __/ |_| | | |___| (_) | (_| | (_| |  __/ |     ____) |  __/ |   \ V /  __/ |     / /_ | |_| |")
+print(" |_|\_\___|\__, | |______\___/ \__, |\__, |\___|_|    |_____/ \___|_|    \_/ \___|_|    |____(_)___/ ")
+print("            __/ |               __/ | __/ |                                                          ")
+print("           |___/               |___/ |___/                                                           ")
+print("Script By DrSquid")
+print("A script for making the client side send what keys they press onto this server.")
 def connect():
     print("[+] Server is up and running.........")
     while True:
